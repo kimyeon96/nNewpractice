76714e4e5ab95874f6cc33c5997932389654f052
kimyeon96 <flflfkfk@naver.com>
2022-03-16 02:12:45 +0900

    Initial commit

---
+++ src/main/java/NewPractice.java
@@ -0,0 +1,9 @@
+public class NewPractice {
+    public static void main(String[] args) {
+        int num=1;
+            System.out.println(num);
+
+            int bNum = 2;
+            System.out.println(bNum);
+    }
+}
