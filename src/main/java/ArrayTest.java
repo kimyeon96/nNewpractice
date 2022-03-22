aff3395c3e71670c53759e3dfce65c289b706723
kimyeon96 <flflfkfk@naver.com>
2022-03-22 21:39:50 +0900

    배열연습

--- src/main/java/ArrayTest.java
+++ src/main/java/ArrayTest.java
@@ -2,10 +2,21 @@ public class ArrayTest {
 
     public static void main(String[] args) {
 
-        int[] numbers = new int[] {0, 1, 2};
+        int[] numbers = {1, 2, 3};
 
-        System.out.println(numbers.length);
+        numbers = new int[3];
 
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
 
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        double[] studentIDs = new double[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
---
+++ src/main/java/BookArray.java
@@ -0,0 +1,13 @@
+public class BookArray {
+    public static void main(String[] args) {
+        private String bookName;
+        private String author;
+
+        public
+        Book[] library = new Book[5];
+
+        for(int i=0; i<library.length; i++){
+            System.out.println(library[i]);
+        }
+    }
+}
