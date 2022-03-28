ff2ce027e7b5b138a992d24496388312c57c77bf
kimyeon96 <flflfkfk@naver.com>
2022-03-28 21:56:13 +0900

    반복문수정

---
+++ src/main/java/classpart.java
@@ -0,0 +1,14 @@
+public class classpart {
+
+    public class Student {
+
+        int studentID;
+        String studentName;
+        int grade;
+        String address;
+
+        public void showStudentInfor() {
+            System.out.println(studentName + "," + address);
+        }
+    }
+}
--- src/main/java/practice99.java
+++ src/main/java/practice99.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class practice99 {
     public static void main(String[] args) {
         System.out.println("안녕하세요?");
         System.out.println("Java입니다.");
         System.out.printf("100 \n");
-        System.out.printf("%d", 100);
-        
+        System.out.printf("%d \n", 100);
+        System.out.printf("%d %d \n", 100, 200);
+        System.out.printf("%d / %d = %f \n", 100, 200, 0.5);
 
+        System.out.printf("%c %c \n", 'a', 'c');
+        System.out.printf("%s \n", "안녕하세요");
+        System.out.printf("%d \n \n", 100);
+        System.out.printf("%d \n", 200);
+        }
     }
-}
