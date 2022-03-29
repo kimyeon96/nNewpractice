45624f4e242feb4eaadfa8047299ee40cb46b02a
kimyeon96 <flflfkfk@naver.com>
2022-03-29 21:36:40 +0900

    챕터3수정

--- src/main/java/Chapter3.java
+++ src/main/java/Chapter3.java
@@ -8,13 +8,5 @@ public class Chapter3 {
         System.out.println(num);
         System.out.println(num2);
 
-
-        int num1 = 10;
-        int i = 2;
-
-        boolean value = ( (num1 = num1 + 10) > 10) && ( (i = i+2) > 10);
-        System.out.println(value);
-        System.out.println(num1);
-        System.out.println(i);
     }
-}
+    }
