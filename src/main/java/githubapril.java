c819f35f27a29b23fff73a6c0855e42ba535df1f
kimyeon96 <flflfkfk@naver.com>
2022-04-01 20:30:32 +0900

    c언어배운거활용

---
+++ src/main/java/githubapril.java
@@ -0,0 +1,30 @@
+public class githubapril {
+    public static void main(String[] args) {
+
+        System.out.printf("%d \n", 123);
+        System.out.printf("%5d \n", 123);
+        System.out.printf("%05d \n", 123);
+
+        System.out.printf("\n");
+
+        System.out.printf("%f \n", 123.45);
+        System.out.printf("%7.1f \n", 123.45);
+        System.out.printf("%7.3f \n", 123.45);
+
+        System.out.printf("\n");
+
+        System.out.printf("%s \n", "Basic-C");
+        System.out.printf("%10s \n", "Basic-C");
+        System.out.printf("%010s \n", "Basic-C");
+
+        double a;
+        float b;
+
+        a = 123.45;
+        b = 200;
+
+        System.out.printf("a의 값 ==> %d \n", a);
+        System.out.printf("b의 값 ==> %f \n", b);
+
+    }
+}
