e8cea84bf1cdc7f81d69516685f09d887d01c39c
kimyeon96 <flflfkfk@naver.com>
2022-04-12 23:16:34 +0900

    22.4.12

---
+++ src/main/java/codeup65.java
@@ -0,0 +1,16 @@
+/*
+import java.util.Scanner;
+public class codeup65 {
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int a;
+        System.out.println("첫번째 수를 입력하세요 : ");
+        a = s.nextInt();
+
+        if (i == *){
+            System.out.println("계산할 연산자를 입력하세요" : )";
+        }
+    }
+}
+*/
--- src/main/java/test03.java
+++ src/main/java/test03.java
@@ -1,10 +1,10 @@
 public class test03 {
     public static void main(String[] args) {
-        for (int i = 1; i < 6; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+        for (int i = 1; i<=4; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.print("0");
             }
-        System.out.println("");
+        System.out.println();
         }
     }
 }
