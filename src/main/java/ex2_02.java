8400b846d81b58c498a8721530a386525c890eae
kimyeon96 <flflfkfk@naver.com>
2022-04-18 01:01:06 +0900

    22.4.18 중간고사 복습

--- src/main/java/ex2_02.java
+++ src/main/java/ex2_02.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
 public class ex2_02 {
-    public static void main(String[] args) {
-        int a;
+    public static void main(String[] args){
+        int a,b;
+        int result;
+
         Scanner s = new Scanner(System.in);
-        System.out.printf("첫번째 계산할 값을 입력하세요 ==> %d");
+        System.out.printf("첫번째 계산할 값을 입력하세요 ==>");
         a = s.nextInt();
+        System.out.printf("두번째 계산할 값을 입력하세요 ==>");
+        b = s.nextInt();
+
+        result = a + b;
+        System.out.println(a + "+" + b + "=" + result);
+
+        result = a - b;
+        System.out.println(a + "-" + b + "=" + result);
+
+        result = a * b;
+        System.out.println(a + "*" + b + "=" + result);
+
+        result = a / b;
+        System.out.println(a + "/" + b + "=" + result);
 
     }
-}
+    }
