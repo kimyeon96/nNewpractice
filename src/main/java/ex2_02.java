862aea10883a7e7472ab857fd457952c9ff1455b
kimyeon96 <flflfkfk@naver.com>
2022-04-17 23:57:59 +0900

    .

---
+++ src/main/java/ex2_02.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ex2_02 {
+    public static void main(String[] args) {
+        int a;
+        Scanner s = new Scanner(System.in);
+        System.out.printf("첫번째 계산할 값을 입력하세요 ==> %d");
+        a = s.nextInt();
+
+    }
+}
---
+++ src/main/java/test055555.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class test055555 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        for(int i=a; i<a; i++) {
+            for(int j=0; j<4; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+
