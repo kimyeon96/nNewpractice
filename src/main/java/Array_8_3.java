a9ae35c3505126c014f26c406bd38f52c08665f1
kimyeon96 <flflfkfk@naver.com>
2022-05-09 23:18:01 +0900

    22.5.9

---
+++ src/main/java/Array_8_3.java
@@ -0,0 +1,16 @@
+//import java.util.Scanner;
+//public class Array_8_3 {
+//    public static void main(String[] args) {
+//        Scanner s = new Scanner(System.in);
+//        int[] aa = new int[10];
+//        int hap = 0;
+//        int i=0;
+//        int a;
+//
+//        while (i <= 10) {
+//            System.out.printf("%d번째 숫자를 입력하세요 : %d",a);
+//            a = s.nextInt();
+//            i++;
+//        }
+//    }
+//}
