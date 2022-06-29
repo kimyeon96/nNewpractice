69a6a9a27946e79a874e1472bf1958115524e893
kimyeon96 <flflfkfk@naver.com>
2022-06-29 23:50:38 +0900

    0629

---
+++ src/main/java/D0629/D0629.java
@@ -0,0 +1,8 @@
+package D0629;
+
+public class D0629 {
+    public static void main(String[] args) {
+        System.out.printf("100+100\n");
+        System.out.printf("%d\n", 100+100);
+    }
+}
---
+++ src/main/java/D0629/D0629_1.java
@@ -0,0 +1,9 @@
+package D0629;
+
+public class D0629_1 {
+    public static void main(String[] args) {
+        System.out.printf("%d / %d = %f \n", 100, 200, 0.5);
+        System.out.printf("%c %c \n", 'a', 'K');
+        System.out.printf("%s %s \n", "IT CookBook", "JAVA");
+    }
+}
