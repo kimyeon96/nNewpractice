5cfb14355ebd6d27dd09b01a41a9865ae48443e2
kimyeon96 <flflfkfk@naver.com>
2022-07-05 23:21:48 +0900

    0705

---
+++ src/main/java/test0705.java
@@ -0,0 +1,11 @@
+public class test0705 {
+    public static void Main(String[] args) {
+        String[] beer = {"Kloud", "Cass", "Asahi", "Guinness", "Heineken"};
+
+        System.out.println(beer[0]); // Kloud
+        System.out.println(beer[1]); // Cass
+        System.out.println(beer[2]); // Asahi
+        System.out.println(beer[3]); // Guinness
+        System.out.println(beer[4]); // Heineken
+    }
+}
